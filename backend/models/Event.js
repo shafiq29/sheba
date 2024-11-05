d98a1beee50634e2fad5fab718609959a62cd781
shafiq <shafiqrobin9@gmail.com>
2024-11-05 19:40:54 +0600

    Features added

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,11 +1,24 @@
-const {DataTypes}   = require('sequelize');
-const sequelize     = require('../config/database');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
 const Event = sequelize.define('Event', {
-    name            : {type: DataTypes.STRING, allowNull: false },
-    totalSeats      : {type: DataTypes.INTEGER, allowNull: false},
-    reservedSeats   : {type: DataTypes.INTEGER, defaultValue: 0},
-    date            : {type: DataTypes.DATE, allowNull: false},
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  totalSeats: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  reservedSeats: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
 });
 
 module.exports = Event;
--- backend/models/Reservation.js
+++ backend/models/Reservation.js
@@ -1,11 +1,18 @@
-const {DataTypes}   = require('sequelize');
-const sequelize     = require('../config/database');
-const Event         = require('../models/Event');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const Event = require('./Event');
 
-const Reservation = sequelize.define('Reservation',{
-    eventId     : {type: DataTypes.INTEGER, references: {model: Event, key: 'id'}},
-    userEmail   : {type: DataTypes.STRING, allowNull: false},
-    seats       : {type: DataTypes.INTEGER, allowNull: false}
+const Reservation = sequelize.define('Reservation', {
+  userEmail: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  seats: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
 });
+
+Reservation.belongsTo(Event); // Associate Reservation with Event
 
 module.exports = Reservation;
